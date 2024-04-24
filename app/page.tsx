05980f6d9f14bfbe98b56f61e3aa85ac5a9f5829
Daniel Henrique <daniel.shenrique@live.com>
2024-04-23 22:07:12 -0300

    inserção da pasta home fora da raiz

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,95 @@
-import styles from "./page.module.css";
-import graphics from "../assets/img/graphics.png";
-import Group2 from "../assets/img/Group 2.png";
-import Rectangle4 from "../assets/img/Rectangle 4.png";
-import Rectangle5 from "../assets/img/Rectangle 5.png";
-import flatlay from "../assets/img/flat-lay-laptop-blue-background 1.png";
-import Logo2 from "../assets/img/Logo2.png";
-import Group4 from "../assets/img/Group 4.png";
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-    <header className={styles.cabecalho}>
-        <nav className={styles.cabecalhoMenu}>
-            <div className={styles.cabecalhoMenuImagem}>
-                <Image className={styles.cabecalhoMenuImagem1} src={graphics} alt="Logo 1"/>
-                <Image className={styles.cabecalhoMenuImagem2} src={Group2} alt="Logo 2"/>
-            </div>
-            <div className={styles.cabecalhoMenuLinks}>
-                <a className={styles.cabecalhoMenuLinks1} href="../app/Lancar_Notas/page.tsx">Lançar notas</a>
-                <Image className={styles.cabecalhoMenuLinksBarra1} src={Rectangle4} alt="Barra de separação"/>
-                <a className={styles.cabecalhoMenuLinks2} href="../app/Cadastro/page.tsx">Cadastro</a>
-                <Image className={styles.cabecalhoMenuLinksBarra2} src={Rectangle5} alt="Barra de separação"/>
-                <a className={styles.cabecalhoMenuLinks3} href="../app/Analise/page.tsx">Análise</a>
-            </div>
-            <div>
-                <a href="app/SignIn/page.tsx"><button className={styles.cabecalhoMenuBotao}>Trocar Usuário</button></a>
-            </div>    
-        </nav>
-    </header>
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
 
-    <main className={styles.conteudo}>
-        <Image className={styles.conteudoBanner} src={flatlay} alt="Banner Bem-vindo"/>
-        <Image className={styles.conteudoLogo} src={Logo2} alt="Banner Bem-vindo"/>
-        <p className={styles.conteudoTexto}>Conectando o presente à excelência do futuro educacional.</p>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
-
-    <footer className={styles.rodape}>
-        <Image className={styles.rodapeImagem} src={Group4} alt="Logo 3"/>
-    </footer>
-  </>  
   );
 }
