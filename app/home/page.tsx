4858d88f1f217ad21c8861cb9e7144418538d05b
Daniel Henrique <daniel.shenrique@live.com>
2024-04-23 22:21:38 -0300

    inserção da pasta home na branch home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
-import graphics from "../assets/img/graphics.png";
-import Group2 from "../assets/img/Group 2.png";
-import Rectangle4 from "../assets/img/Rectangle 4.png";
-import Rectangle5 from "../assets/img/Rectangle 5.png";
-import flatlay from "../assets/img/flat-lay-laptop-blue-background 1.png";
-import Logo2 from "../assets/img/Logo2.png";
-import Group4 from "../assets/img/Group 4.png";
+import graphics from "../../assets/img/graphics.png";
+import Group2 from "../../assets/img/Group2.png";
+import Rectangle4 from "../../assets/img/Rectangle4.png";
+import Rectangle5 from "../../assets/img/Rectangle5.png";
+import flatlay from "../../assets/img/flat-lay-laptop-blue-background1.png";
+import Logo2 from "../../assets/img/Logo2.png";
+import Group4 from "../../assets/img/Group4.png";
 import Image from "next/image";
 
 export default function Home() {
@@ -14,8 +14,8 @@ export default function Home() {
     <header className={styles.cabecalho}>
         <nav className={styles.cabecalhoMenu}>
             <div className={styles.cabecalhoMenuImagem}>
-                <Image className={styles.cabecalhoMenuImagem1} src={graphics} alt="Logo 1"/>
-                <Image className={styles.cabecalhoMenuImagem2} src={Group2} alt="Logo 2"/>
+                <Image className={styles.cabecalhoMenuImagem1} src={graphics} alt="Logo1"/>
+                <Image className={styles.cabecalhoMenuImagem2} src={Group2} alt="Logo2"/>
             </div>
             <div className={styles.cabecalhoMenuLinks}>
                 <a className={styles.cabecalhoMenuLinks1} href="../app/Lancar_Notas/page.tsx">Lançar notas</a>
